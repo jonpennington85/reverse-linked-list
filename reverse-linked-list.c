020942d2cf4ef819eaf748feedbaf1c9756bd6ff
Jonathan Pennington <jonathan.s.pennington@wmich.edu>
2017-07-29 07:22:11 -0400

    Added comments and cleaned up the code. Also reduced number from 2000 to 200

--- reverse-linked-list.c
+++ reverse-linked-list.c
@@ -1,6 +1,20 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                               *
+ * Author: Jonathan Pennington                                   *
+ *                                                               *
+ * A simple program that reverses a linked list using pointers.  *
+ *                                                               *
+ * Implemented in C                                              *
+ *                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LIST_MEM  600
+#define LIST_SIZE 200
+
+// Just a sample struct with an int to prove it works
 struct linkedList {
 	int number;
 	struct linkedList * next;
@@ -8,7 +22,8 @@ struct linkedList {
 
 int main(int argc, char ** argv) {
 
-	struct linkedList *list = malloc(600);
+	// We will use all these pointers to point to various parts of the list
+	struct linkedList *list = malloc(LIST_MEM);
 	struct linkedList *beginningList=list;
 	struct linkedList *currentNode;
 	struct linkedList *reverseList;
@@ -17,30 +32,33 @@ int main(int argc, char ** argv) {
 	int numberofNodes = 0;
 	int i=0;
 
-	for (i = 0; i < 2000; i++) {
+	// Populate the linked list. No cheating with arrays
+	for (i = 0; i < LIST_SIZE; i++) {
 	    currentNode = list;
 	    currentNode->number = i;
 	    list += sizeof(struct linkedList);
-	    if (i < 1999) currentNode->next = list;
+	    if (i < LIST_SIZE-1) currentNode->next = list;
 	}
 
 	currentNode = beginningList;
 
+	// Print out the list to prove it works
 	while (currentNode != 0x0) {
-	printf("%d\n",currentNode->number);
+		printf("%d\n",currentNode->number);
 		currentNode = currentNode->next;
 	}
 
 	currentNode=beginningList;
+	previousNode=beginningList;
 
-	previousNode=currentNode;
-
+	// Now we find the last node, keeping track of its previous
 	while(currentNode->next!=0x0){
 		previousNode=currentNode;
 		currentNode=currentNode->next;
 		numberofNodes++;
 	}
 
+	// Set up the first node in reverseList
 	reverseList=currentNode;
 	beginningReverse=currentNode;
 	reverseList->next=previousNode;
@@ -48,6 +66,9 @@ int main(int argc, char ** argv) {
 	currentNode=beginningList;
 	numberofNodes--;
 
+	// Continue going to the end of the linked list and adding
+	// the last node to the reversed list until there are no more
+	// Appears to be in O(n!)...very inefficient
 	while(numberofNodes!=0){
 		for(i=0;i<numberofNodes-1;i++){
 			currentNode = currentNode->next;
@@ -58,8 +79,10 @@ int main(int argc, char ** argv) {
 		numberofNodes--;
 	}
 
+	// Set the last node to a null address space
 	reverseList->next=0x0;
 
+	// Print out the results to prove it works
 	reverseList=beginningReverse;
 	while(reverseList!=0x0){
 		printf("%d\n",reverseList->number);
